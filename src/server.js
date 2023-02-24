c15dbec661ef77e602424ab00fded42739961205
cynthia-Kam <abijurucyn@gmail.com>
2023-02-24 11:56:10 +0200

    added a  blog controller with CRUD operations that successfully work with database

--- src/server.js
+++ src/server.js
@@ -1,1 +1,32 @@
-import express from "express"
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import allRoutes from "./routes/allRoutes.js"
+
+mongoose.set('strictQuery', false);
+
+dotenv.config();
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use("/", allRoutes)
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+//database connection instances
+const con=()=>mongoose.connect(process.env.MONGODB_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const startServer=()=>app.listen(port);
+
+Promise.all([con(),startServer()])
+.then(()=>{
+    console.log(`MONGODB connected and server listening at http://${host}:${port}`)
+})
+.catch((err)=>console.log(err))
